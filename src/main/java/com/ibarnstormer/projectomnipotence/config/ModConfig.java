4ed27c4b8779fc47e6e349d404d8889f16e70cfd
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2025-08-13 20:17:23 -0400

    Added workaround for https://github.com/google/gson/issues/1794

--- src/main/java/com/ibarnstormer/projectomnipotence/config/ModConfig.java
+++ src/main/java/com/ibarnstormer/projectomnipotence/config/ModConfig.java
@@ -2,11 +2,19 @@ package com.ibarnstormer.projectomnipotence.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 import com.ibarnstormer.projectomnipotence.Main;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -14,7 +22,7 @@ import java.util.*;
 public class ModConfig {
 
     private static final Gson GSON = new GsonBuilder()
-            .disableHtmlEscaping().setLenient().setPrettyPrinting().create();
+            .disableHtmlEscaping().setLenient().setPrettyPrinting().registerTypeAdapterFactory(new RecordTypeAdapterFactory()).create();
 
     private final String _comment_version = "Config version, set to -1 to prevent config updates.";
     private int version = Main.CONFIG_VERSION;
@@ -122,6 +130,65 @@ public class ModConfig {
         config.damageReflectionBlackList.addAll(freshConfig.damageReflectionBlackList);
         config.enlightenmentBlackList.addAll(freshConfig.enlightenmentBlackList);
         config.convertUponEnlightened.putAll(freshConfig.convertUponEnlightened);
+
+    }
+
+    // Taken from https://github.com/google/gson/issues/1794
+    public static class RecordTypeAdapterFactory implements TypeAdapterFactory {
+
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            @SuppressWarnings("unchecked")
+            Class<T> clazz = (Class<T>) type.getRawType();
+            if (!clazz.isRecord()) {
+                return null;
+            }
+            TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+
+            return new TypeAdapter<T>() {
+                @Override
+                public void write(JsonWriter out, T value) throws IOException {
+                    delegate.write(out, value);
+                }
+
+                @Override
+                public T read(JsonReader reader) throws IOException {
+                    if (reader.peek() == JsonToken.NULL) {
+                        reader.nextNull();
+                        return null;
+                    } else {
+                        var recordComponents = clazz.getRecordComponents();
+                        var typeMap = new HashMap<String,TypeToken<?>>();
+                        for (int i = 0; i < recordComponents.length; i++) {
+                            typeMap.put(recordComponents[i].getName(), TypeToken.get(recordComponents[i].getGenericType()));
+                        }
+                        var argsMap = new HashMap<String,Object>();
+                        reader.beginObject();
+                        while (reader.hasNext()) {
+                            String name = reader.nextName();
+                            argsMap.put(name, gson.getAdapter(typeMap.get(name)).read(reader));
+                        }
+                        reader.endObject();
+
+                        var argTypes = new Class<?>[recordComponents.length];
+                        var args = new Object[recordComponents.length];
+                        for (int i = 0; i < recordComponents.length; i++) {
+                            argTypes[i] = recordComponents[i].getType();
+                            args[i] = argsMap.get(recordComponents[i].getName());
+                        }
+                        Constructor<T> constructor;
+                        try {
+                            constructor = clazz.getDeclaredConstructor(argTypes);
+                            constructor.setAccessible(true);
+                            return constructor.newInstance(args);
+                        } catch (NoSuchMethodException | InstantiationException | SecurityException |
+                                 IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            };
+        }
 
     }
 
